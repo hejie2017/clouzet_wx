f038553c0beb75bd2edb95eb4ac9679cae8555a7
DESKTOP-I0FL7EQ\TG3D_001 <glwms@outlook.com>
2017-09-30 15:35:50 +0800

    no message

--- pages/scan/conn/conn.js
+++ pages/scan/conn/conn.js
@@ -10,7 +10,18 @@ Page({
     startPoint:[0,0],
     moveIndexTemp:0,
     imageIndex:0,
-    imageUrl: "http://localhost/wechat/SH_16_angle_test0000.png",
+    imageUrl: "http://202.168.133.150/wechat/SH_16_angle_test0000.png",
+    Disabled:true,
+    CollarCircumference:"",
+    ChestCircumference:"",
+    CenterBackNecktoCrotchLength: "",
+    NarrowWaistCircumference: "",
+    LowHipCircumference: "",
+    BackCrossShoulderWidth: "",
+    LeftUpperArmCircumference: "",
+    LeftArmLength: "",
+    LeftThighCircumference: "",
+    LeftInseamLength: "",
   },
 
   onLoad: function (opt) {
@@ -30,7 +41,7 @@ Page({
   },
 
   turnleft: function () {
-    var imageStr = "http://localhost/wechat/SH_16_angle_test00";
+    var imageStr = "http://202.168.133.150/wechat/SH_16_angle_test00";
     this.data.imageIndex = this.data.imageIndex -1;
     if (this.data.imageIndex < 0) this.data.imageIndex = 15;
 
@@ -49,7 +60,7 @@ Page({
   },
 
   turnright: function () {
-    var imageStr = "http://localhost/wechat/SH_16_angle_test00";
+    var imageStr = "http://202.168.133.150/wechat/SH_16_angle_test00";
     this.data.imageIndex = this.data.imageIndex + 1;
     if (this.data.imageIndex > 15) this.data.imageIndex = this.data.imageIndex - 15;
     if (this.data.imageIndex < 0) this.data.imageIndex = this.data.imageIndex + 15;
@@ -78,7 +89,7 @@ Page({
     // if (this.data.moveIndexTemp == moveIndex) return;
     this.data.moveIndexTemp = moveIndex;
     this.data.imageIndex =  - moveIndex;
-    var imageStr = "http://localhost/wechat/SH_16_angle_test00";
+    var imageStr = "http://202.168.133.150/wechat/SH_16_angle_test00";
     if (this.data.imageIndex > 15) this.data.imageIndex = this.data.imageIndex - 16;
     if (this.data.imageIndex < 0) this.data.imageIndex = this.data.imageIndex + 16;
 
@@ -88,125 +99,26 @@ Page({
     else {
       imageStr = imageStr + this.data.imageIndex + ".png";
     }
-     console.log(imageStr);
     this.setData({
       imageUrl: imageStr
     })
 
+    // wx.setStorageSync(imageStr, 'data2')
+
   },
 
-  // INITIAL: function () {
-  //   var that = this;
-  //   var doMain = getApp().globalData.doMain;
-  //   var api_key = getApp().globalData.api_key;
-  //   var httpPath = "/api/v1/scanners/";
-  //   var http = doMain + httpPath + getApp().globalData.scannerid + "/?apikey=" + api_key;
-  //   console.log(http);
-  //   wx.request({
-  //     url: http,
-  //     data: {
-  //       "version": 2,
-  //       "session_key": getApp().globalData.sessionkey
-  //     },
-  //     header: {
-  //       "Content-Type": "application/json",
-  //       "X-User-Access-Token": getApp().globalData._access_token
-  //     },
-  //     method: "POST",
-  //     success: function (res) {
-  //       console.log(res)
-  //       if (res.statusCode == 201) {
-  //         that.setData({
-  //           tid: res.data.tid,
-  //           infoMess: "初始化扫描机成功！",
-  //         })
-  //         that.CONFIRM();
-  //       } else {
-  //         that.setData({
-  //           infoMess: "errno:" + res.statusCode + " msg:" + res.errMsg
-  //         })
-  //       }
-  //     },
-  //     fail: function (err) {
-  //       that.setData({
-  //         infoMess: err,
-  //       })
-  //       console.log(err)
-  //     }
-  //   })
-  // },
-
-  // CONFIRM: function () {
-  //   var that = this;
-  //   var doMain = getApp().globalData.doMain;
-  //   var api_key = getApp().globalData.api_key;
-  //   var httpPath = "/api/v1/scanners/";
-  //   var http = doMain + httpPath + that.data.scannerid + '/'+ that.data.tid + "/confirm?apikey=" + api_key;
-  //   console.log(http);
-  //   wx.request({
-  //     url: http,
-  //     header: {
-  //       "Content-Type": "application/json",
-  //       "X-User-Access-Token": that.data._access_token
-  //     },
-  //     method: "POST",
-  //     success: function (res) {
-  //       console.log(res)
-  //       if (res.statusCode == 200) {
-  //         that.setData({
-  //           // tid: res.data.tid,
-  //           infoMess: "初始化已经确认！",
-  //         })
-  //       } else {
-  //         that.setData({
-  //           infoMess: "errno:" + res.statusCode + " msg:" + res.errMsg,
-  //         })
-  //       }
-  //     },
-  //     fail: function (err) {
-  //       that.setData({
-  //         infoMess: err,
-  //       })
-  //       console.log(err)
-  //     }
-  //   })
-  // },
-
-  // START: function () {
-  //   var that = this;
-  //   var doMain = getApp().globalData.doMain;
-  //   var api_key = getApp().globalData.api_key;
-  //   var httpPath = "/api/v1/scanners/";
-  //   var http = doMain + httpPath + that.data.scannerid + '/' + that.data.tid + "/start?apikey=" + api_key;
-  //   console.log(http);
-  //   wx.request({
-  //     url: http,
-  //     header: {
-  //       "Content-Type": "application/json",
-  //       "X-User-Access-Token": that.data._access_token
-  //     },
-  //     method: "POST",
-  //     success: function (res) {
-  //       console.log(res)
-  //       if (res.statusCode == 200) {
-  //         that.setData({
-  //           infoMess: "启动扫描机！",
-  //         })
-  //         that.WAITFORSCANNING();
-  //       } else {
-  //         that.setData({
-  //           infoMess: "errno:" + res.statusCode + " msg:" + res.errMsg,
-  //         })
-  //       }
-  //     },
-  //     fail: function (err) {
-  //       that.setData({
-  //         infoMess: err,
-  //       })
-  //       console.log(err)
-  //     }
-  //   })
-  // },
+  save: function (e) {
+    console.log('开始保存')
+    wx.setStorage({
+      key: 'key1',
+      data: 'data1',
+      success: function (res) {
+        console.log('异步保存成功')
+      }
+    })
+    wx.setStorageSync('key2', 'data2')
+    console.log('同步保存成功')
+  },
 
   // CANCEL: function () {
   //   var that = this;
@@ -336,11 +248,21 @@ Page({
         console.log(res)
         if (res.statusCode == 200) {
           that.setData({
-            infoMess: "获得扫描结果！",
-            url: "",
-            local_url: "",
-            md5: ""
+            infoMess: "获得人体属性！",
+            CollarCircumference: res.data.measurement["Collar Circumference"].value,
+            ChestCircumference: res.data.measurement["Chest Circumference"].value,
+            CenterBackNecktoCrotchLength: res.data.measurement["Center Back Neck to Crotch Length"].value,
+            NarrowWaistCircumference: res.data.measurement["Narrow Waist Circumference"].value,
+            LowHipCircumference: res.data.measurement["Low Hip Circumference"].value,
+            BackCrossShoulderWidth: res.data.measurement["Back Cross Shoulder Width"].value,
+            LeftUpperArmCircumference: res.data.measurement["Left Upper Arm Circumference"].value,
+            LeftArmLength: res.data.measurement["Left Arm Length"].value,
+            LeftThighCircumference: res.data.measurement["Left Thigh Circumference"].value,
+            LeftInseamLength: res.data.measurement["Left Inseam Length"].value,
+
+            Disabled: false,
           })
+
         } else {
           that.setData({
             infoMess: "errno:" + res.statusCode + " msg:" + res.errMsg,
